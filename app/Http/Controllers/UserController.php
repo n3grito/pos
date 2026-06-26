c6689fb8e63e4f7b13da0439439d816a86ed1e39
Admin <admin@pos.com>
2026-06-26 12:38:56 -0400

    Fix missing Warehouse import in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Branch;
 use App\Models\User;
+use App\Models\Warehouse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
